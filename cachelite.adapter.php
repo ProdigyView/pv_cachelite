cefaf475be0466847c25aefc5df2c94a6ccb2e4f
ProdigyView <contact@prodigyview.com>
2012-02-07 19:23:25 -0500

    PVCache nit

--- cachelite.adapter.php
+++ cachelite.adapter.php
@@ -1,10 +1,14 @@
 <?php
 /**
  * The adapter will replace the functionality the PVCache methods and adapt them to use the 
- * methods in the CacheLite class.
+ * methods in the CacheLite class. In other words, CacheLite becomes similiar to a dependent
+ * of the cache class.
  * 
  * The adapter is an example of aspect oriented programming.
  */
+
+PVCache::addAdapter('PVCache', 'init', 'CacheLite');
+ 
 PVCache::addAdapter('PVCache', 'writeCache', 'CacheLite');
 
 PVCache::addAdapter('PVCache', 'readCache', 'CacheLite');
